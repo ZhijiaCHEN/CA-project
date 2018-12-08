07f4d6a26a65390e081dd5dbccb578cbc6248db4
Zhijia Chen <zhijia.chen@temple.edu>
2018-12-08 13:32:29 -0500

    remove invalid scripts file as well

--- simplesim-3.0/make-zhijia.sh
+++
@@ -1,6 +0,0 @@
-rm cache.c cache.h sim-outder.c sim-cache.c sim-cache.o cache.o
-ln -s ~/CA-project/simplesim-3.0/cache.h cache.h
-ln -s ~/CA-project/simplesim-3.0/cache.c cache.c
-ln -s ~/CA-project/simplesim-3.0/sim-outorder.c sim-outorder.c
-ln -s ~/CA-project/simplesim-3.0/sim-cache.c sim-cache.c
-make
