e7b22d7991777aeada91aeb27ea2e4fb4ff11414
Zhijia Chen <zhijia.chen@temple.edu>
2018-12-13 02:00:12 -0500

    .

--- simplesim-3.0/mymake.sh
+++ simplesim-3.0/mymake.sh
@@ -1,4 +1,4 @@
-rm -f cache.c cache.h sim-cache.c main.c sim.h sim-cache.o cache.o testmake.sh mymake.sh myconfig.cfg mytest.sh
+rm -f cache.c cache.h sim-cache.c main.c sim.h sim-cache.o cache.o testmake.sh mymake.sh myconfig.cfg mytest.sh single-core-config.cfg double-core-config.cfg
 ln -s ~/CA-project/simplesim-3.0/main.c main.c
 ln -s ~/CA-project/simplesim-3.0/sim.h sim.h
 ln -s ~/CA-project/simplesim-3.0/cache.h cache.h
@@ -6,6 +6,8 @@ ln -s ~/CA-project/simplesim-3.0/cache.c cache.c
 ln -s ~/CA-project/simplesim-3.0/sim-cache.c sim-cache.c
 ln -s ~/CA-project/simplesim-3.0/testmake.sh testmake.sh
 ln -s ~/CA-project/simplesim-3.0/mymake.sh mymake.sh
-ln -s ~/CA-project/simplesim-3.0/myconfig.cfg myconfig.cfg
 ln -s ~/CA-project/simplesim-3.0/mytest.sh mytest.sh
+ln -s ~/CA-project/simplesim-3.0/myconfig.cfg myconfig.cfg
+ln -s ~/CA-project/simplesim-3.0/single-core-config.cfg single-core-config.cfg
+ln -s ~/CA-project/simplesim-3.0/double-core-config.cfg double-core-config.cfg
 make sim-cache
--- simplesim-3.0/mytest.sh
+++ simplesim-3.0/mytest.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
 reset
 mymake.sh
-sim-cache -config myconfig.cfg testprog1 hello world : testprog2 foo bar hello fox
+if ["$#" -ne 1] 
+then
+    echo "using cofiguration myconfig.cfg"
+    sim-cache -config myconfig.cfg testprog1 hello world : testprog2 foo bar hello fox
+else
+    echo "using cofiguration $1"
+    sim-cache -config $1 testprog1 hello world : testprog2 foo bar hello fox 
+fi
